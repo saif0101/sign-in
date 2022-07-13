a1b7261081f211f300621e85317f4397d7351918
saif0101 <saif13@cse.pstu.ac.bd>
2022-07-13 18:37:55 +0600

    all good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut,createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react';
+
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, setUSer] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: '',
+    password:''
+  })
+
+  const provider = new GoogleAuthProvider();
+
+  const handleSingIn = () => {
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, email, photoURL ,password} = result.user;
+        const singnIned = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+          password: password
+          
+        }
+        setUSer(singnIned);
+        console.log(result);
+
+      })
+      .cath(error => {
+        console.log(error);
+        console.log(error.message);
+      })
+
+  }
+  const handleSingOut = () => {
+    const auth = getAuth(app);
+    signOut(auth).then(() => {
+      const singnOut = {
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: '',
+        error :'',
+        success: false
+      }
+      setUSer(singnOut);
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+
+  }
+  const handleChange = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.name === "password") {
+      const isValidPass = e.target.value.length > 6;
+       const passValid =  /\d{1}/.test(e.target.value);
+      isFieldValid = isValidPass && passValid;
+    }
+    if (isFieldValid){
+      const newUSerInfo = {...user};
+      newUSerInfo[e.target.name]= e.target.value;
+      console.log(newUSerInfo);
+      setUSer(newUSerInfo);
+    }
+  }
+  const handleSubmit=(e)=>{
+    if ( user.email && user.password){ 
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, user.email, user.password)
+   .then(res => {
+     // Signed in 
+    //  const user = userCredential.user;
+     const newUSerInfo ={...user};
+     newUSerInfo.error ='';
+     newUSerInfo.success = true;
+     setUSer(newUSerInfo);
+     // ...
+   })
+   .catch((error) => {
+   
+     const newUSerInfo = {...user};
+     newUSerInfo.error = error.message;
+     newUSerInfo.success = false ;
+     setUSer(newUSerInfo);
+  
+     // ..
+   });
+  
+  }
+  e.preventDefault();
+  }
+
+
+    return (
+      <div className="App">
+
+
+        {user.isSignedIn ? <button onClick={handleSingOut}> Sing out </button> : <button onClick={handleSingIn}> Sing In</button>
+
+        }
+        {
+          user.isSignedIn &&
+          <div>
+            <h1> Welcome {user.name} </h1>
+            <h4> Your email : {user.email}</h4>
+            <img src={user.photo} alt="photoURL" />
+          </div>
+        }
+
+        <h4> Sign in Here</h4>
+        <form onSubmit={handleSubmit}>
+          <input type="email" name="email" onBlur={handleChange} placeholder='type your email' required />
+          <br />
+          <input type="password" name="password"  onBlur={handleChange} placeholder='type your password' required />
+          <br />
+          <input type="submit"  value=" submit" />
+        </form>
+        {user.error && <p style={{color: 'red'}} > The email is in Used</p>}
+        {user.success && <p style={{color: 'green'}} > User Created Succesfully</p>}
     </div>
   );
 }
-
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqCK20yWRi27UDiIa43-FPdHiLuRHyHnk",
+    authDomain: "sign-in-practice-a4818.firebaseapp.com",
+    projectId: "sign-in-practice-a4818",
+    storageBucket: "sign-in-practice-a4818.appspot.com",
+    messagingSenderId: "666287662658",
+    appId: "1:666287662658:web:64ccf3d96332b644baafdb"
+  };
+
+  export default firebaseConfig;
